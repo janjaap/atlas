ae37b74134f5cd63b7775bb76965e97763518411
Adrian Tudorache <a.p.tudorache@gmail.com>
2018-11-14 12:44:16 +0100

    DP-6065 add base route on bakkie

--- scripts/bakkie.sh
+++ scripts/bakkie.sh
@@ -22,8 +22,13 @@ fi
 rm -rf ${OUTDIR}/*
 cp -r dist/* ${OUTDIR}
 
+# change the base route for test
+BASE_ROUTE=/atlas/builds/$1/
+sed -i 's|<base href="\/">|<base href=\"'"$BASE_ROUTE"'\">|' ${OUTDIR}/index.html
+
 echo Generate new index.html for all branches
 echo '<h1>atlas links</h1>' > /var/www/html/atlas/index.html
 echo "<h2>Generated at `date`</h2>" >> /var/www/html/atlas/index.html
 find /var/www/html/atlas -name index.html -printf "%T+\t%p\n" | sort -r | grep 'atlas' | sed 's/^.*\/atlas/\/atlas/' | sed 's/\/index.html//' | sed 's/^.*$/<p><a href="\0">\0<\/a><p>/' >> /var/www/html/atlas/index.html
 echo Done
+
